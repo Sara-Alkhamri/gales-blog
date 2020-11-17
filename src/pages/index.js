795cae40ec1e660f2e4c360c5b4c0c5751354c75
Sara Alkhamri <s.alkhamri@gmail.com>
2020-11-17 11:32:12 -0800

    Added main logo image to index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,7 +15,11 @@ class IndexPage extends React.Component {
           title="Home"
           keywords={[`blog`, `gatsby`, `javascript`, `react`]}
         />
-        <img style={{ margin: 0 }} src="./GatsbyScene.svg" alt="Gatsby Scene" />
+        <img
+          style={{ margin: 0 }}
+          src="./I.CreateHealthy.png"
+          alt="I.Create.Healthy"
+        />
         <h1>
           Hey people{" "}
           <span role="img" aria-label="wave emoji">
