d967e8768ea2c045e4b1bfa049fde91e62d7a5b2
Sara Alkhamri <s.alkhamri@gmail.com>
2020-11-16 17:48:00 -0800

    Site title edit

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,7 @@ import Button from "../components/button"
 
 class IndexPage extends React.Component {
   render() {
-    const siteTitle = "Gatsby Starter Personal Website"
+    const siteTitle = "I. Create. Healthy"
 
     return (
       <Layout location={this.props.location} title={siteTitle}>
