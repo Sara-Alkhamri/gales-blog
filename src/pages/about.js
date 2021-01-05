07ee2159aa4c3e2c7370f30b59afcf1a9c7b5c3e
Sara Alkhamri <s.alkhamri@gmail.com>
2021-01-05 11:42:13 -0800

    Updated About text

--- src/pages/about.js
+++ src/pages/about.js
@@ -20,32 +20,35 @@ export default function About() {
         </Link>
       </div>
       <h1>The Journey…</h1>
-      <p>
-        Nobody said it would be easy but it is worth it! I wanted to curate a
-        community of what I like to call new growth. Over the years we have
-        gotten further and further away from the nature of things. Everything
-        from medication to swipe left, swipe right ends in someone’s instant
-        gratification. New growth requires a new way of understanding, a new way
-        of doing things given through the guidance of our ancestral roots. g{" "}
-        <br></br> I created I Create Healthy as a cliff notes of sorts for women
-        like me. I had so many questions but I just wanted quick answers from
-        someone I could trust. Kinda of like that wise ol woman that always has
-        a natural cure for everything and you just trusted her because she was
-        never wrong. That’s the type of vibes I am kicking out. I am here to
-        help. I am the nerd that does all the research and you’re the jock that
-        I let copy my notes! Lol. Our body and the fucti I want you to
-        understand that it is all a struggle. Fguay possible. re out who is for
-        your and those sare the ohnes that wsnt you to win. Everyone else….they
-        want to see you fail in the nicest way.
-      </p>
-      {/* <p>
-        Such wow. Very React. is simply dummy text of the printing and
-        typesetting industry. Lorem Ipsum has been the industry's standard dummy
-        text ever since the 1500s, when an unknown printer took a galley of type
-        and scrambled it to make a type specimen book. It has survived not only
-        five centuries, but also the leap into electronic typesetting, remaining
-        essentially unchanged.{" "}
-      </p> */}
+      <div
+        style={{
+          display: `flex`,
+          flexWrap: `wrap`,
+        }}
+      >
+        <p>
+          This journey started in 2016 when I had the Ah Ha moment that you can
+          heal yourself naturally. I started taking small steps researching into
+          how this is. In doing my research, and being the test dummy, I have
+          come to learn quite a few things which has inspired my new passion of
+          becoming the healthiest version of oneself inside and out….even if
+          sometimes we stumble along the way. It’s about balance right!? This is
+          a space for “like minds” to come together and chat. For people like me
+          who are passionate about our bodies, our minds, and our souls.
+        </p>
+        <img
+          style={{ margin: `1rem`, width: `28rem` }}
+          src="../about.jpg"
+          alt="about image"
+        />
+        <p style={{ width: `500px`, marginLeft: `4rem` }}>
+          My passion has become holistic health and wellness and I am here to
+          help educate, encourage and help promote the best life that YOU can
+          offer to yourself! I hope this website inspires you to create, nurture
+          and to think. Welcome.
+        </p>
+      </div>
+
       {/* <img style={{ width: `22rem` }} src={aboutImage} alt="about" /> */}
     </Layout>
   )
